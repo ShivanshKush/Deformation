713800a937de2a8db2378602f440d6cb3d460743
ShivanshKush <64358424+ShivanshKush@users.noreply.github.com>
2020-05-31 17:29:23 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Deformation
-Global car Association
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ is touching.js
@@ -0,0 +1,3 @@
+function isTouching(Object1,Object2){
+    Object1.x-Object2.x < (Object2.width+Object1.width)/2;
+  }
